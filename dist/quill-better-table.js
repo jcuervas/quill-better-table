fd447ab2402aa75405187e6ca5b124d1b732304d
Javier Cuervas <javier.cuervas@gmail.com>
2023-10-15 01:22:48 +0200

    Update quill-better-table.js

--- dist/quill-better-table.js
+++ dist/quill-better-table.js
@@ -2976,8 +2976,8 @@ class quill_better_table_BetterTable extends Module {
     // expected my binding callback excute first
     // I changed the order of binding callbacks
 
-    let thisBinding = quill.keyboard.bindings['Backspace'].pop();
-    quill.keyboard.bindings['Backspace'].splice(0, 1, thisBinding); // add Matchers to match and render quill-better-table for initialization
+    let thisBinding = quill.keyboard.bindings['8'].pop();
+    quill.keyboard.bindings['8'].splice(0, 1, thisBinding); // add Matchers to match and render quill-better-table for initialization
     // or pasting
 
     quill.clipboard.addMatcher('td', matchTableCell);
